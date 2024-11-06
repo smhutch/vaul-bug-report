b27c97034f1152a03cfc24847588fe825d639277
Scott Hutcheson <scott@smhutch.co.uk>
2024-11-06 14:55:10 +0000

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Drawer } from "vaul";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Vaul demo</h1>
